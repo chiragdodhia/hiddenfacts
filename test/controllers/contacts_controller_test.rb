cf19913a43615152103106f90b6975ea9fe4197e
Chirag Dodhia <chiragdodhia1997@gmail.com>
2020-06-09 17:21:06 +0530

    options_change

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
   test "should create contact" do
-    post contacts_url, params: { 'first-name': "John", 'last-name': "Doe", 'message': "Hii I am John Doe", 'phone': "9478474787", email: "test@gmail.com" }
+    post contacts_url, params: { 'first-name': "John", 'last-name': "Doe", 'message': "Hii I am John Doe", 'phone': "9478474787", 'email': "test@gmail.com" }
     assert_response :found
   end
 
